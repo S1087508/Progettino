5267c17a9107a946b82a598c50c18cd726fc0a7e
Rumeysa Nur Gulesin <s1087508@studenti.univpm.it>
2020-10-01 22:39:47 +0200

    commit di prova

---
+++ analisiPaesi/src/main/java/covid19/analisiPaesi/AnalisiPaesiApplication.java
@@ -0,0 +1,13 @@
+package covid19.analisiPaesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisiPaesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisiPaesiApplication.class, args);
+	}
+
+}
---
+++ analisiPaesi/src/test/java/covid19/analisiPaesi/AnalisiPaesiApplicationTests.java
@@ -0,0 +1,13 @@
+package covid19.analisiPaesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalisiPaesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
